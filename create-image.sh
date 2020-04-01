f8382a1001600add9fc3aac2d450d3455817c726
Binarysearch <desenlace.net@gmail.com>
2020-04-01 19:27:53 +0200

    bug

--- create-image.sh
+++ create-image.sh
@@ -2,5 +2,6 @@
 
 
 docker build --rm -f Dockerfile -t docker-private.genetius.es/webrtcexample:latest . &&
+(docker container rm docker-private.genetius.es/webrtcexample -f || true) &&
 docker run --rm -d  -p 8443:8443/tcp docker-private.genetius.es/webrtcexample:latest
 #docker push docker-private.genetius.es/webrtcexample:latest
